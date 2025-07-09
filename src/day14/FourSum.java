4de8f8e2f83288f41bc781e2b4bef933c6199054
Wilbur <lwb980210@gmail.com>
2025-07-09 14:39:49 +1000

    四数之和 排序+双指针 与三数不同的是，四数需要固定前两个数，然后再双指针指向后两个数。

---
+++ src/day14/FourSum.java
@@ -0,0 +1,42 @@
+package day14;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        // 排序
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        // 固定前两个数，使用双指针--比三数之和多一层循环
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1, right = n - 1;
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,2,2,2,2};
+        System.out.println(new FourSum().fourSum(nums, 8));
+    }
+}
